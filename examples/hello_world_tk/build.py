ae725f0e92479bcc8450ee8836d2d46d2ac28f9a
BuvinJ <buvintech@gmail.com>
2019-01-05 15:35:35 -0500

    Revised example.

--- examples/hello_world_tk/build.py
+++ examples/hello_world_tk/build.py
@@ -1,18 +1,20 @@
 from distbuilder import PyToBinInstallerProcess, ConfigFactory
 
-factory = ConfigFactory()
-factory.productName      = "Hello World Example"
-factory.description      = "A Distribution Builder Example"
-factory.companyTradeName = "Some Company"
-factory.companyLegalName = "Some Company Inc."    
-factory.binaryName       = "HelloWorld"
-factory.isGui            = True        
-factory.entryPointPy     = "hello.py"  
-factory.iconFilePath     = "demo.ico" 
-factory.version          = (1,0,0,0)
-factory.setupName        = "HelloWorldSetup.exe"
+f = configFactory = ConfigFactory()
+f.productName      = "Hello World Example"
+f.description      = "A Distribution Builder Example"
+f.companyTradeName = "Some Company"
+f.companyLegalName = "Some Company Inc."    
+f.binaryName       = "HelloWorld"
+f.isGui            = True        
+f.entryPointPy     = "hello.py"  
+f.iconFilePath     = "demo.ico" 
+f.version          = (1,0,0,0)
+f.setupName        = "HelloWorldSetup.exe"
 
 class BuildProcess( PyToBinInstallerProcess ):
     def onOpyConfig( self, cfg ):    
         cfg.external_modules.extend( [ 'tkinter', 'tkinter.ttk' ] )
-BuildProcess( factory, isObfuscating=True, isMovedToDesktop=True ).run()       
+p = BuildProcess( configFactory, isObfuscating=True, isMovedToDesktop=True )
+p.isTestingInstall = True
+p.run()       
