0a405bc53363b2a4fa14dd91a5ee83e7273790a0
BuvinJ <buvintech@gmail.com>
2021-03-06 10:09:15 -0500

    Refactored Win Scripts Hello Resources example.

--- examples/hello_win_scripts/hello_resources.py
+++ examples/hello_win_scripts/hello_resources.py
@@ -1,6 +1,27 @@
 from distbuilder import( WinScriptToBinPackageProcess, ConfigFactory,
                          ExecutableScript )
 
+def openTextFileScript( scriptExt, fileName ):
+    scriptOptions = {
+          ExecutableScript.BATCH_EXT : 
+            'echo. > "%userprofile%\Desktop\{fileName}"'
+        , ExecutableScript.PS_EXT :
+            [
+                  '$DesktopPath = [Environment]::GetFolderPath( "Desktop" )'
+                , '$FilePath    = Join-Path $DesktopPath {fileName}'
+                , 'New-Item $FilePath'
+            ]
+        , ExecutableScript.VBS_EXT:
+r''' 
+Dim oShell : Set oShell = CreateObject( "WScript.Shell" )
+oShell.Exec( "%ComSpec% /c start """" ""%windir%\system32\notepad.exe"" ""{fileName}""" )
+'''
+    }
+    script = scriptOptions.get( scriptExt )
+    if not script: raise Exception( "Invalid Script Type!" )    
+    return ExecutableScript( "openTextFile", extension=scriptExt, script=script,
+                             replacements={ "fileName":fileName } ) 
+
 f = configFactory  = ConfigFactory()
 f.productName      = "Hello Script Resources Example"
 f.description      = "A Distribution Builder Example"
@@ -10,12 +31,8 @@ f.binaryName       = "HelloScriptResources"
 f.version          = (1,0,0,0)
 f.iconFilePath     = "../hello_world_tk/demo.ico" 
 f.distResources    = ["../hello_world_tk/LICENSE.TXT"]
-f.entryPointScript = "test.vbs"
-f.entryPointScript = ExecutableScript( "openLicense", extension="vbs", script=(
-r''' 
-Dim oShell : Set oShell = CreateObject( "WScript.Shell" )
-oShell.Exec( "%ComSpec% /c start """" ""%windir%\system32\notepad.exe"" ""{fileName}""" )
-'''), replacements={"fileName":"LICENSE.TXT"} )
+f.entryPointScript = openTextFileScript( ExecutableScript.VBS_EXT, 
+                                         "LICENSE.TXT" )
  
 p = WinScriptToBinPackageProcess( configFactory, isDesktopTarget=True,
                                   isZipped=False )
