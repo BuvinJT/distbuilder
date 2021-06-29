5c3562fbb9a73e6faec726f8d5fe70a36bf42233
BuvinJ <buvintech@gmail.com>
2021-06-29 14:06:23 -0400

    Began modifying init for new WIP documentation generation.

--- distbuilder/__init__.py
+++ distbuilder/__init__.py
@@ -1,41 +1,11 @@
+# DOCS > NULL
 from ._version import __version__ 
 
-from distbuilder.process import( 
-      ConfigFactory 
-    , PyToBinPackageProcess 
-    , IExpressPackageProcess
-    , PyToBinInstallerProcess
-    , IExpressInstallerProcess
-    , RobustInstallerProcess 
-)
+# DOCS > Config Factory.md
+from distbuilder.process import ConfigFactory
 
-from distbuilder.log import( 
-    Logger, startLogging, stopLogging, isLogging, log )
-       
-from distbuilder.py_installer import( 
-      PyInstallerConfig 
-    , PyInstSpec 
-    , PyInstHook 
-    , pyScriptToExe
-    , makePyInstSpec 
-    , installPyInstaller 
-    , uninstallPyInstaller 
-    , PyInstallerVersion 
-    , PyInstallerMajorVer 
-    , PyInstallerMajorMinorVer
-)
-
-from distbuilder.iexpress import( 
-      IExpressConfig
-    , batchScriptToExe
-    , powerShellScriptToExe
-    , vbScriptToExe
-    , jScriptToExe  
-    , iExpResPath 
-    , iExpEmbResPath
-    , iExpLibPath
-)
-
+# DOCS > Qt Installer Framework.md
+from distbuilder.process import RobustInstallerProcess
 from distbuilder.qt_installer import( 
       QtIfwConfig 
     , QtIfwConfigXml 
@@ -104,16 +74,55 @@ from distbuilder.qt_installer import(
     , QT_IFW_PRE_INSTALL 
     , QT_IFW_POST_INSTALL 
 )
-
-from distbuilder.pip_installer import( 
-      PipConfig 
-    , updatePip 
-    , installLibraries 
-    , installLibrary 
-    , uninstallLibrary 
-    , vcsUrl
+       
+# DOCS > PyInstaller.md       
+from distbuilder.process import( 
+      PyToBinPackageProcess 
+    , PyToBinInstallerProcess
+)
+from distbuilder.py_installer import( 
+      PyInstallerConfig 
+    , PyInstSpec 
+    , PyInstHook 
+    , pyScriptToExe
+    , makePyInstSpec 
+    , installPyInstaller 
+    , uninstallPyInstaller 
+    , PyInstallerVersion 
+    , PyInstallerMajorVer 
+    , PyInstallerMajorMinorVer
 )
 
+# DOCS > IExpress.md
+from distbuilder.process import( 
+      IExpressPackageProcess
+    , IExpressInstallerProcess
+)
+from distbuilder.iexpress import( 
+      IExpressConfig
+    , batchScriptToExe
+    , powerShellScriptToExe
+    , vbScriptToExe
+    , jScriptToExe  
+    , iExpResPath 
+    , iExpEmbResPath
+    , iExpLibPath
+)
+
+# DOCS > Code Signing.md
+from distbuilder.code_sign import( 
+      SelfSignedCertConfig 
+    , CodeSignConfig 
+    , TrustInstallerBuilderProcess 
+    , SIGNTOOL_PATH_ENV_VAR 
+    , MAKECERT_PATH_ENV_VAR 
+    , PVK2PFX_PATH_ENV_VAR 
+    , trustCertInstallerConfigFactory 
+    , generateTrustCerts 
+    , signExe 
+)
+
+# DOCS > Python Obfuscation.md
 from distbuilder.opy_library import( 
       OpyConfigExt as OpyConfig 
     , OpyPatch 
@@ -126,37 +135,40 @@ from distbuilder.opy_library import(
 
 ExtLibHandling = OpyConfig.ExtLibHandling 
 
-from distbuilder.code_sign import( 
-      SelfSignedCertConfig 
-    , CodeSignConfig 
-    , TrustInstallerBuilderProcess 
-    , SIGNTOOL_PATH_ENV_VAR 
-    , MAKECERT_PATH_ENV_VAR 
-    , PVK2PFX_PATH_ENV_VAR 
-    , trustCertInstallerConfigFactory 
-    , generateTrustCerts 
-    , signExe 
+# DOCS > Python pip.md
+from distbuilder.pip_installer import( 
+      PipConfig 
+    , updatePip 
+    , installLibraries 
+    , installLibrary 
+    , uninstallLibrary 
+    , vcsUrl
 )
+
+# DOCS > Qt CPP.md 
+""" DOCS > VIRTUAL
+from distbuilder.qt_cpp import (
+    QtCppConfig, 
+    qmakeInit, installDeployTools, renameExe, 
+    qmakeMasterConfigFactory, qmakePackageConfigFactory, 
+    qmakeArgs, qmakeArgParser 
+)
+"""
             
+# DOCS > Logging.md            
+from distbuilder.log import( 
+    Logger, startLogging, stopLogging, isLogging, log )
+
+# DOCS > Utility Classes.md            
 from distbuilder.util import(
       RawConfigParser, ConfigParser, SafeConfigParser
     , ExecutableScript    
     , WindowsExeVersionInfo        
-    , IS_WINDOWS 
-    , IS_LINUX 
-    , IS_MACOS 
-    , PY2 
-    , PY3 
-    , BIT_CONTEXT 
-    , IS_32_BIT_CONTEXT 
-    , IS_64_BIT_CONTEXT 
-    , IS_ARM_CPU 
-    , IS_INTEL_CPU 
-    , THIS_DIR 
-    , CURRENT_USER, ALL_USERS 
-    , DEBUG_ENV_VAR_NAME, DEBUG_ENV_VAR_VALUE
-    , ALL 
-    , absPath 
+)
+
+# DOCS > Utility Functions.md            
+from distbuilder.util import(
+      absPath 
     , homePath
     , desktopPath     
     , toNativePath 
@@ -235,3 +247,20 @@ from distbuilder.util import(
 def assertBuilderVer( ver ): 
     assertMinVer( __version__, ver, descr="Distribution Builder Library" )
 
+# DOCS > Utility Constants.md            
+from distbuilder.util import(
+      IS_WINDOWS 
+    , IS_LINUX 
+    , IS_MACOS 
+    , PY2 
+    , PY3 
+    , BIT_CONTEXT 
+    , IS_32_BIT_CONTEXT 
+    , IS_64_BIT_CONTEXT 
+    , IS_ARM_CPU 
+    , IS_INTEL_CPU 
+    , THIS_DIR 
+    , CURRENT_USER, ALL_USERS 
+    , DEBUG_ENV_VAR_NAME, DEBUG_ENV_VAR_VALUE
+    , ALL 
+)
